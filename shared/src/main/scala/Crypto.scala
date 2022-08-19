cc40d682cfb79be1e894e0a0230d78cdba84ce03
VzxPLnHqr <>
2022-08-19 22:24:43 +0000

    more descriptive require messages

--- shared/src/main/scala/Crypto.scala
+++ shared/src/main/scala/Crypto.scala
@@ -73,8 +73,8 @@ object Crypto extends CryptoPlatform {
     *   serialized public key, in compressed format (33 bytes)
     */
   case class PublicKey(value: ByteVector) extends PublicKeyPlatform(value) {
-    require(value.length == 33)
-    require(isPubKeyValidLax(value))
+    require(value.length == 33,s"pubkey is ${value.length} bytes but should be 33 bytes")
+    require(isPubKeyValidLax(value),"pubkey is not valid")
 
     def hash160: ByteVector = Crypto.hash160(value)
 
