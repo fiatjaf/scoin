7953c98f56d0cdd2713dec9794b99bd20d598d8b
fiatjaf <fiatjaf@gmail.com>
2022-08-21 12:03:57 -0300

    style changes on hc codecs to match lightning message codecs.

--- shared/src/main/scala/hc/HostedChannelCodecs.scala
+++ shared/src/main/scala/hc/HostedChannelCodecs.scala
@@ -12,102 +12,86 @@ import scoin.hc.HostedChannelTags._
 
 object HostedChannelCodecs {
   val invokeHostedChannelCodec = (
-    // @formatter:off
-    (bytes32 withContext "chainHash") ::
-    (varsizebinarydata withContext "refundScriptPubKey") ::
-    (varsizebinarydata withContext "secret")
-    // @formatter:on
+    ("chainHash" | bytes32) ::
+      ("refundScriptPubKey" | varsizebinarydata) ::
+      ("secret" | varsizebinarydata)
   ).as[InvokeHostedChannel]
 
   val initHostedChannelCodec = (
-    // @formatter:off
-    (uint64 withContext "maxHtlcValueInFlightMsat") ::
-    (millisatoshi withContext "htlcMinimumMsat") ::
-    (uint16 withContext "maxAcceptedHtlcs") ::
-    (millisatoshi withContext "channelCapacityMsat") ::
-    (millisatoshi withContext "initialClientBalanceMsat") ::
-    (listOfN(uint16, uint16) withContext "features")
-    // @formatter:on
+    ("maxHtlcValueInFlightMsat" | uint64) ::
+      ("htlcMinimumMsat" | millisatoshi) ::
+      ("maxAcceptedHtlcs" | uint16) ::
+      ("channelCapacityMsat" | millisatoshi) ::
+      ("initialClientBalanceMsat" | millisatoshi) ::
+      ("features" | listOfN(uint16, uint16))
   ).as[InitHostedChannel]
 
   val hostedChannelBrandingCodec = (
-    // @formatter:off
-    (rgb withContext "rgbColor") ::
-    (optional(bool8, varsizebinarydata) withContext "pngIcon") ::
-    (variableSizeBytes(uint16, utf8) withContext "contactInfo")
-    // @formatter:on
+    ("rgbColor" | rgb) ::
+      ("pngIcon" | optional(bool8, varsizebinarydata)) ::
+      ("contactInfo" | variableSizeBytes(uint16, utf8))
   ).as[HostedChannelBranding]
 
   lazy val lastCrossSignedStateCodec = (
-    // @formatter:off
-    (bool8 withContext "isHost") ::
-    (varsizebinarydata withContext "refundScriptPubKey") ::
-    (lengthDelimited(initHostedChannelCodec) withContext "initHostedChannel") ::
-    (uint32 withContext "blockDay") ::
-    (millisatoshi withContext "localBalanceMsat") ::
-    (millisatoshi withContext "remoteBalanceMsat") ::
-    (uint32 withContext "localUpdates") ::
-    (uint32 withContext "remoteUpdates") ::
-    (listOfN(
-      uint16,
-      lengthDelimited(updateAddHtlcCodec)
-    ) withContext "incomingHtlcs") ::
-    (listOfN(
-      uint16,
-      lengthDelimited(updateAddHtlcCodec)
-    ) withContext "outgoingHtlcs") ::
-    (bytes64 withContext "remoteSigOfLocal") ::
-    (bytes64 withContext "localSigOfRemote")
-    // @formatter:on
+    ("isHost" | bool8) ::
+      ("refundScriptPubKey" | varsizebinarydata) ::
+      ("initHostedChannel" | lengthDelimited(initHostedChannelCodec)) ::
+      ("blockDay" | uint32) ::
+      ("localBalanceMsat" | millisatoshi) ::
+      ("remoteBalanceMsat" | millisatoshi) ::
+      ("localUpdates" | uint32) ::
+      ("remoteUpdates" | uint32) ::
+      ("incomingHtlcs" | listOfN(
+        uint16,
+        lengthDelimited(updateAddHtlcCodec)
+      )) ::
+      ("outgoingHtlcs" | listOfN(
+        uint16,
+        lengthDelimited(updateAddHtlcCodec)
+      )) ::
+      ("remoteSigOfLocal" | bytes64) ::
+      ("localSigOfRemote" | bytes64)
   ).as[LastCrossSignedState]
 
   val stateUpdateCodec = (
-    // @formatter:off
-    (uint32 withContext "blockDay") ::
-    (uint32 withContext "localUpdates") ::
-    (uint32 withContext "remoteUpdates") ::
-    (bytes64 withContext "localSigOfRemoteLCSS")
-    // @formatter:on
+    ("blockDay" | uint32) ::
+      ("localUpdates" | uint32) ::
+      ("remoteUpdates" | uint32) ::
+      ("localSigOfRemoteLCSS" | bytes64)
   ).as[StateUpdate]
 
   val stateOverrideCodec = (
-    // @formatter:off
-    (uint32 withContext "blockDay") ::
-    (millisatoshi withContext "localBalanceMsat") ::
-    (uint32 withContext "localUpdates") ::
-    (uint32 withContext "remoteUpdates") ::
-    (bytes64 withContext "localSigOfRemoteLCSS")
-    // @formatter:ofn
+    ("blockDay" | uint32) ::
+      ("localBalanceMsat" | millisatoshi) ::
+      ("localUpdates" | uint32) ::
+      ("remoteUpdates" | uint32) ::
+      ("localSigOfRemoteLCSS" | bytes64)
   ).as[StateOverride]
 
   val announcementSignatureCodec = (
-    // @formatter:off
-    (bytes64 withContext "nodeSignature") ::
-    (bool8 withContext "wantsReply")
-    // @formatter:on
+    ("nodeSignature" | bytes64) ::
+      ("wantsReply" | bool8)
   ).as[AnnouncementSignature]
 
   val resizeChannelCodec = (
-    // @formatter:off
-    (satoshi withContext "newCapacity") ::
-    (bytes64 withContext "clientSig")
-    // @formatter:on
+    ("newCapacity" | satoshi) ::
+      ("clientSig" | bytes64)
   ).as[ResizeChannel]
 
   val askBrandingInfoCodec =
-    (bytes32 withContext "chainHash").as[AskBrandingInfo]
+    ("chainHash" | bytes32).as[AskBrandingInfo]
 
   val queryPublicHostedChannelsCodec =
-    (bytes32 withContext "chainHash").as[QueryPublicHostedChannels]
+    ("chainHash" | bytes32).as[QueryPublicHostedChannels]
 
   val replyPublicHostedChannelsEndCodec =
-    (bytes32 withContext "chainHash").as[ReplyPublicHostedChannelsEnd]
+    ("chainHash" | bytes32).as[ReplyPublicHostedChannelsEnd]
 
   val queryPreimagesCodec =
-    (listOfN(uint16, bytes32) withContext "hashes").as[QueryPreimages]
+    ("hashes" | listOfN(uint16, bytes32)).as[QueryPreimages]
 
   val replyPreimagesCodec =
-    (listOfN(uint16, bytes32) withContext "preimages").as[ReplyPreimages]
+    ("preimages" | listOfN(uint16, bytes32)).as[ReplyPreimages]
 
   def decodeHostedMessage(
       tag: Int,
