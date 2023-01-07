fa0937351cdc35685db609e78edc85f50261ec24
VzxPLnHqr <>
2023-01-07 21:54:12 +0000

    fix non-exhaustive match warning

--- shared/src/main/scala/ScriptTree.scala
+++ shared/src/main/scala/ScriptTree.scala
@@ -1,6 +1,9 @@
 package scoin
 import scodec.bits._
 import java.io.ByteArrayOutputStream
+import scoin.ScriptTree.Branch
+import scoin.ScriptTree.Branch
+import scoin.ScriptTree.Leaf
 
 /**
  * leaf of a script tree used to create and spend tapscript transactions
@@ -23,18 +26,17 @@ class ScriptLeaf(val id: Int, val script: ByteVector, val leafVersion: Int) {
 /**
  * Simple binary tree structure
  */
-sealed abstract class ScriptTree[T] {
-    case class Leaf[T](value: T) extends ScriptTree[T]
-    case class Branch[T](left: ScriptTree[T], right: ScriptTree[T]) extends ScriptTree[T]
-}
+sealed abstract class ScriptTree[T]
 
 object ScriptTree {
+    case class Leaf[T](value: T) extends ScriptTree[T]
+    case class Branch[T](left: ScriptTree[T], right: ScriptTree[T]) extends ScriptTree[T]
     /**
      * @return the hash of the input merkle tree
      */
     def hash(tree: ScriptTree[ScriptLeaf]): ByteVector32 = tree match {
-        case tree.Leaf(value) => value.hash
-        case tree.Branch(left,right) => {
+        case Leaf(value) => value.hash
+        case Branch(left,right) => {
             val h1 = hash(left)
             val h2 = hash(right)
             Crypto.taggedHash((if (LexicographicalOrdering.isLessThan(h1, h2)) h1 ++ h2 else h2 ++ h1), "TapBranch")
