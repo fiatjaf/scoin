80117c31d0f71a2a8a724f227d7f4b17670408c8
fiatjaf <fiatjaf@gmail.com>
2023-02-07 13:43:09 -0300

    testing with a random anyprevout tx from aj towns's bitcoind's random tests.

--- shared/src/test/scala/AnyprevoutTest.scala
+++ shared/src/test/scala/AnyprevoutTest.scala
@@ -119,6 +119,61 @@ object AnyprevoutTest extends TestSuite {
         ScriptFlags.STANDARD_SCRIPT_VERIFY_FLAGS | ScriptFlags.SCRIPT_VERIFY_ANYPREVOUT
       )
     }
+
+    test("test anyprevout tx from bitcoin core") {
+      val tx = Transaction.read(
+        "0200000003e829065a8fd64effda7c9594f69b081ae7a9c7be409febbe7d0c2e58f98e28e38d0000000035f3c2def654f9e7c38287bf0c6c5063373f586fd2ac616d5493bfede3e81e387097237b2602000000cac81eb9ade232d72dd5b7db1f05e1c554568d0a4ccac5ed58bf4a7557f330ee35007f273900000000a1f7ee8d04cd9fce00000000001976a9144f958f5c26468454b69371c6d87cf28e82abd2f988ac580200000000000017a914a4393d67fc274c97516a4485ca28b80d4b3841bb8758020000000000001976a9144f958f5c26468454b69371c6d87cf28e82abd2f988ac580200000000000017a9145604c2ca6e2885c3b910c40dfe32b8061d0391d7878d3b2031"
+      )
+      val prevouts = List(
+        "c9445d0000000000225120f23bc61131c0ac2ea89f0da22436d997d8c652699595c35782ac14031cda51e6",
+        "2c7b500000000000225120cb685fedefd1ec5d3840edd599c4b1b5b41ae6344c867acf64a5eee0d2fdd983",
+        "8b7a2300000000002251209fbe5b1f1d931841d27039dde19a7d5788edac4c78385598b78e2b87f874a036"
+      ).map(ByteVector.fromValidHex(_)).map { bytes =>
+        val amount = Satoshi(
+          bytes
+            .take(8)
+            .toLong(signed = false, ordering = ByteOrdering.LittleEndian)
+        )
+        val scriptpubkey = bytes.drop(8)
+        TxOut(amount, scriptpubkey)
+      }
+
+      Transaction.correctlySpends(
+        tx.updateWitness(
+          0,
+          ScriptWitness(
+            Seq(
+              hex"2cff94196c3aff46587a32a0912bd714e1911a9dddc9cbd6354366283fe42cb928e9b4770d2b21fe8725e42078b0c7f5c1439bb1a491fbbfe93b44023b90e416",
+              hex"21012fb71bb52006d3ee7ba33ebde4d9b51ffb591164de77904e1e539665ec293c8aac",
+              hex"c0588cc77bf9a7f8ee775555198695d84869c65b3e851e1f57b2eb77234b61983529db27987037304adfdb102d3cb61b54ae8bfadb94bdc16826c95f59d5d912dea695d86cd4530169ad2062da5a41fcb536a518b92c6f7d6dc2b85c6e9fa5c279"
+            )
+          )
+        ),
+        tx.txIn.zipWithIndex
+          .map((vin, i) => (vin.outPoint -> prevouts(i)))
+          .toMap,
+        ScriptFlags.STANDARD_SCRIPT_VERIFY_FLAGS | ScriptFlags.SCRIPT_VERIFY_ANYPREVOUT
+      )
+
+      assertFails {
+        Transaction.correctlySpends(
+          tx.updateWitness(
+            0,
+            ScriptWitness(
+              Seq(
+                hex"fcf8af63eef1ed3506f4540904604664703a1dc37de86c1d1f8ec91ac8ede7c72cb8b82f1059ed41cc1e131ca407a194078e83909b2ca18bde983d77e323153a8100",
+                hex"2101d8eb0bef7e626ca279d89d5d08853ca64f582c4c8365d01fb9e5c4b411390118ac",
+                hex"c01956f80262744377be3d5335758b33ce33ed3876f71aa30ad7174c033bfc62acc9480d8a0f90cbab609791834477d738d70edc1830979b2bc60c24475c61e59d8f9e2f79f6a62852e4872903803b01bb3c95996aacf5d15f8224bfaa8cdde3e0934e5c9de419cb968cf8b98103c7865e02f72242a8c1fcb4574b43b5d80a07ff"
+              )
+            )
+          ),
+          tx.txIn.zipWithIndex
+            .map((vin, i) => (vin.outPoint -> prevouts(i)))
+            .toMap,
+          ScriptFlags.STANDARD_SCRIPT_VERIFY_FLAGS | ScriptFlags.SCRIPT_VERIFY_ANYPREVOUT
+        )
+      }
+    }
   }
 
   // helper function so we can copy/paste easier from ACINQ's test code
