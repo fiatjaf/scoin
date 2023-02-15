931c0732b8bea350e12e7b5fcb34b30693dd7cc0
VzxPLnHqr <>
2023-02-15 20:27:52 +0000

    musig2 - key aggregation tests pass for valid cases, not yet testing error cases

--- shared/src/main/scala/Musig2.scala
+++ shared/src/main/scala/Musig2.scala
@@ -1,12 +1,32 @@
 package scoin
 
 import Crypto._
+import scodec.bits.ByteVector
 
 object Musig2 {
+
   /**
    *  Musig2 draft specification
     * https://github.com/jonasnick/bips/blob/musig2-squashed/bip-musig2.mediawiki
     */
+
+  /**
+    * KeyGen Context for Musig2 signing session.
+    *
+    * @param pointQ 
+    *   The point Q representing the aggregate and potentially tweaked public 
+    *   key: an elliptic curve point
+    * @param gacc
+    *   `1 or -1 mod n` where `n` is the group order.
+    * @param accumulatedTweak
+    *   The accumulated tweak tacc: an integer with `0 ≤ accumuatedTweak < n` 
+    *   where `n` is the group order.
+    */
+  final case class KeyGenCtx ( 
+    pointQ: PublicKey, 
+    gacc: BigInt,
+    accumulatedTweak: BigInt
+  )
 
   /**
     * Sort a list of public keys in lexographical order
@@ -14,5 +34,57 @@ object Musig2 {
     * @param pubkeys
     */
   def keySort(pubkeys: List[PublicKey]): List[PublicKey] = pubkeys.sortBy(_.value)
+
+  def keyAgg(pubkeys: List[PublicKey]): KeyGenCtx = {
+    // note: max list size is 2^32 - 1
+    val pk2 = getSecondKey(pubkeys.map(_.value))
+    // if this function is being called, then we assume all PublicKeys in the
+    // list are valid public keys
+    val coefficients = pubkeys.map(i => keyAggCoeffInternal(pubkeys,i.value,pk2))
+    val pointQ = pubkeys.zip(coefficients).map{
+      case (pubkey_i, coeff_i) => pubkey_i.multiply(PrivateKey(coeff_i))
+    }.reduce{
+      case (lhs, rhs) => lhs + rhs
+    }
+
+    // ensure that the aggregate point is not the point at infinity
+    require(pointQ.isValid, "invalid aggregate public key")
+    KeyGenCtx(pointQ, gacc = BigInt(1), accumulatedTweak = BigInt(0))
+  }
+
+  private[scoin] def hashKeys(pubkeys: List[PublicKey]): ByteVector32 =
+    taggedHash(
+      pubkeys.foldLeft(ByteVector.empty){
+        case (accum, i) => accum ++ i.value
+      },
+      "KeyAgg list"
+    )
+
+  private[scoin] def getSecondKey(pubkeys: List[ByteVector]): ByteVector =
+    pubkeys.headOption match {
+      case None => throw new IllegalArgumentException("list of public keys cannot be empty")
+      case Some(pk0) => pubkeys.dropWhile(_ == pk0).headOption match {
+        case None => ByteVector.fill(33)(0.toByte)
+        case Some(pkj) => pkj
+      }
+    }
+
+  private[scoin] def keyAggCoeff(pubkeys: List[ByteVector], pubkey: ByteVector): BigInt 
+    = keyAggCoeffInternal(pubkeys.map(PublicKey(_)),pubkey, getSecondKey(pubkeys))
+
+  private[scoin] def keyAggCoeffInternal(
+                        pubkeys: List[PublicKey], 
+                        pubkey: ByteVector, 
+                        pubkey2: ByteVector
+                      ): BigInt = {
+                        val L = hashKeys(pubkeys)
+                        if( pubkey == pubkey2 ) 
+                          BigInt(1)
+                        else
+                          BigInt(
+                            taggedHash(L ++ pubkey,"KeyAgg coefficient").toHex,
+                            radix = 16
+                          ).mod(N)
+                      }
 
 }
--- shared/src/test/scala/Musig2Test.scala
+++ shared/src/test/scala/Musig2Test.scala
@@ -4,10 +4,12 @@ import scoin._
 import utest._
 import scodec.bits.ByteVector
 import Crypto._
+import scoin.TaprootTest.assertFails
 object Musig2Test extends TestSuite {
   // tests and algorithms mostly from
   // https://github.com/jonasnick/bips/blob/musig2-squashed/bip-musig2.mediawiki
   val tests = Tests {
+
     test("musig2 - lexographic key sorting") {
       val pubkeys = List(
         "02DD308AFEC5777E13121FA72B9CC1B7CC0139715309B086C960E18FD969774EB8",
@@ -26,6 +28,56 @@ object Musig2Test extends TestSuite {
       ).map(i => PublicKey(ByteVector.fromValidHex(i)))
 
       assert((Musig2.keySort(pubkeys)) == sorted_pubkeys)
+    }
+
+    test("musig2 - key aggregation") {
+      // https://github.com/jonasnick/bips/blob/musig2-squashed/bip-musig2/vectors/key_agg_vectors.json
+
+      // note: some of these pubkeys are invalid
+      val pubkeys_bytes = List(
+        "02F9308A019258C31049344F85F89D5229B531C845836F99B08601F113BCE036F9",
+        "03DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659",
+        "023590A94E768F8E1815C2F24B4D80A8E3149316C3518CE7B7AD338368D038CA66",
+        "020000000000000000000000000000000000000000000000000000000000000005",
+        "02FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC30",
+        "04F9308A019258C31049344F85F89D5229B531C845836F99B08601F113BCE036F9",
+        "03935F972DA013F80AE011890FA89B67A27B7BE6CCB24D3274D18B2D4067F261A9"
+      ).map(i => ByteVector.fromValidHex(i))
+
+      // note: one of these tweaks is out of range
+      val tweaks = List(
+        "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
+        "252E4BD67410A76CDF933D30EAA1608214037F1B105A013ECCD3C5C184A6110B"
+      ).map(i => BigInt(i,16))
+      
+      val pubkeys012 = pubkeys_bytes(0) :: pubkeys_bytes(1) :: pubkeys_bytes(2) :: Nil
+      assert(
+        (Musig2.keyAgg(pubkeys012.map(PublicKey(_))).pointQ.xonly.value.toHex.toUpperCase) 
+        == 
+        "90539EEDE565F5D054F32CC0C220126889ED1E5D193BAF15AEF344FE59D4610C"
+      )
+
+      // same pubkeys as above, different order
+      val pubkeys210 = pubkeys_bytes(2) :: pubkeys_bytes(1) :: pubkeys_bytes(0) :: Nil
+      assert(
+        (Musig2.keyAgg(pubkeys210.map(PublicKey(_))).pointQ.xonly.value.toHex.toUpperCase) 
+        == 
+        "6204DE8B083426DC6EAF9502D27024D53FC826BF7D2012148A0575435DF54B2B"
+      )
+
+      val pubkeys000 = pubkeys_bytes(0) :: pubkeys_bytes(0) :: pubkeys_bytes(0) :: Nil
+      assert(
+        (Musig2.keyAgg(pubkeys000.map(PublicKey(_))).pointQ.xonly.value.toHex.toUpperCase) 
+        == 
+        "B436E3BAD62B8CD409969A224731C193D051162D8C5AE8B109306127DA3AA935"
+      )
+
+      val pubkeys0011 = pubkeys_bytes(0) :: pubkeys_bytes(0) :: pubkeys_bytes(1) :: pubkeys_bytes(1) :: Nil
+      assert(
+        (Musig2.keyAgg(pubkeys0011.map(PublicKey(_))).pointQ.xonly.value.toHex.toUpperCase) 
+        == 
+        "69BC22BFA5D106306E48A20679DE1D7389386124D07571D0D872686028C26A3E"
+      )
     }
   }
 }
